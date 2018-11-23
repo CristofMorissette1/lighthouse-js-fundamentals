ac8043ddd39c443c0a30ca53582201a0aab0d920
CristofMorissette1 <cristofmorissette@hotmail.com>
2018-11-23 17:27:33 +0000

    Looping problems completed

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+
+while (i <= ingredients.length) {
+  console.log(ingredients[i]);
+  i++
+}
+
+for (let i = 0; i <= ingredients.length; i += 1) {
+  console.log(ingredients[i]);
+}
+
+for (let i = 7; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
